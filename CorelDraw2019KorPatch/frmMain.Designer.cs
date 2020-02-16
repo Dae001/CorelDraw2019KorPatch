f61c985b559dfac955cd70cf68b4fb40e84315b0
SeokHyunShin <dae001hyun001@msn.com>
2020-02-16 20:23:49 +0900

    _default.cdws파일이 존재하면 CorelDraw2019가 설치된걸로 간주

---
+++ CorelDraw2019KorPatch/frmMain.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace CorelDraw2019KorPatch
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("맑은 고딕", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnStart.Location = new System.Drawing.Point(119, 64);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(117, 46);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start...";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(365, 193);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CorelDraw2019 한글화 v1.0";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+    }
+}
+
---
+++ CorelDraw2019KorPatch/frmMain.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace CorelDraw2019KorPatch
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            ResourceSave();
+        }
+
+        private void ResourceSave()
+        {
+            string strFolder = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) +
+                                   @"\AppData\Roaming\Corel\CorelDRAW Graphics Suite 2019\Draw\Workspace\_default.cdws";
+            // _default.cdws파일이 존재하면 CorelDraw2019가 설치된걸로 간주
+            FileInfo fileInfo = new FileInfo(strFolder);
+            if (fileInfo.Exists)
+            {
+                try
+                {
+                    var _default = Properties.Resources._default;
+
+                    FileStream file = new FileStream(strFolder, FileMode.Create);
+
+                    file.Write(_default, 0, _default.Length);
+                    file.Close();
+                    FinalBeep("CorelDraw2019가 한글화가 되었습니다!");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString(), "Error Accessing Resourc!");
+                }
+            }
+            else
+            {
+                FinalBeep("CorelDraw2019를 설치하고 실행하세요!");
+            }
+        }
+
+            private void FinalBeep(string msg)
+        {
+            Console.Beep();
+            btnStart.Location = new Point(30, btnStart.Location.Y);
+            btnStart.Size = new Size(303, 46);
+            btnStart.ForeColor = Color.Red;
+            btnStart.Text = msg;
+            Delay(2000);
+            this.Close();
+        }
+
+        private static DateTime Delay(int MS)
+        {
+            DateTime ThisMoment = DateTime.Now;
+            TimeSpan duration = new TimeSpan(0, 0, 0, 0, MS);
+            DateTime AfterWards = ThisMoment.Add(duration);
+
+            while (AfterWards >= ThisMoment)
+            {
+                System.Windows.Forms.Application.DoEvents();
+                ThisMoment = DateTime.Now;
+            }
+
+            return DateTime.Now;
+        }
+
+        //private void Filecopy()
+        //{
+        //    string fileName = "_default.cdws";
+        //    string sourcePath = Environment.CurrentDirectory;
+        //    string targetPath = Environment.CurrentDirectory + @"\AA";
+        //    // string targetPath = @"C:\Users\user\AppData\Roaming\Corel\CorelDRAW Graphics Suite 2019\Draw\Workspace";
+        //    string sourceFile = Path.Combine(sourcePath, fileName);
+        //    string destFile = Path.Combine(targetPath, fileName);
+
+        //    Directory.CreateDirectory(targetPath);
+        //    File.Copy(sourceFile, destFile, true);
+        //}
+
+
+    }
+}
